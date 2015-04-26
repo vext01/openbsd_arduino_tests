a8239b4343f9f34c9294f1c518fa3f4c6fd30d6f
Edd Barrett <vext01@gmail.com>
2015-04-26 11:42:20 +0100

    Tweaks to compilation.

--- test_arduino.py
+++ test_arduino.py
@@ -1,31 +1,49 @@
+#!/usr/bin/env python2.7
+# When hacking, please use PEP8
+
 import sh
 import shutil
 import os
 
-WORK_DIR = "work"
-INO_DIR = "inos"
 HERE = os.path.abspath(os.path.dirname(__file__))
+WORK_DIR = os.path.join(HERE, "work")
+INO_DIR = os.path.join(HERE, "inos")
+
 
 def mk_project(proj_name, ino_file=None):
     """ builds a fresh arduino project and optionally copies in an ino """
+
+    if not os.path.exists(WORK_DIR):
+        os.mkdir(WORK_DIR)
+
     proj_dir = os.path.join(WORK_DIR, proj_name)
     if os.path.exists(proj_dir):
         shutil.rmtree(proj_dir)
+
     os.chdir(WORK_DIR)
     sh.arduinoproject(proj_name)
     os.chdir(HERE)
+
     if ino_file:
         shutil.copy(os.path.join(INO_DIR, ino_file), proj_dir)
     return proj_dir
 
-def compile(proj_dir):
+
+def compile(proj_dir, make_flags=None):
+    """ comples the project, optionally passing flags to make """
+    if not make_flags:
+        make_flags = []
     os.chdir(proj_dir)
-    sh.make()
+    print(sh.make(*make_flags))
     os.chdir(HERE)
+
+    # existing hex file indicates a succeeded compile
+    proj_name = os.path.basename(proj_dir)
+    assert os.path.exists(os.path.join(proj_dir, "applet", proj_name + ".hex"))
+
+# >>> Tests below
 
 def test_blank():
     """ blank project """
-    d = mk_project("blank")
-    compile(d)
-
-
+    proj = mk_project("blank")
+    compile(proj)
